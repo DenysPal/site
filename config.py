dd560a913a9fed8ae13b932fa4a4fc8f288dface
Саша Темчур <sashatemcur@MacBook-Pro-Sasa.local>
2025-07-06 17:20:30 +0300

    d

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_TOKEN = "7986296580:AAFPPanjd-fd4uvyfsoKXt1jaOgLc3J_DBw"  # Main bot token
 BOT_TOKEN = API_TOKEN  # Alias for server scripts
 
 # Telegram Group and Admin IDs
-ADMIN_GROUP_ID = -4842159343  # Main admin group
+ADMIN_GROUP_ID = -48572341  # Main admin group
 GROUP_ID = ADMIN_GROUP_ID    # Alias for server scripts
 ADMIN_ID = 7973971109        # Main admin user
 ADMIN_IDS = {7450357530, 7237511252, 6637345609, 7973971109}  # Set of admin user IDs 
