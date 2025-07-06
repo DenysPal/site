9f3eabafcb46d4e5ad7af4ac85ac130a67513d6b
denys <palianytsia@denys>
2025-07-06 15:59:10 +0300

    frf

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # Centralized configuration for bot and admin credentials
 
 # Telegram Bot Token
-API_TOKEN = "5619487724:AAFeBptlX1aJ9IEAFLMUXN3JZBImJ35quWk"  # Main bot token
+API_TOKEN = "7986296580:AAFPPanjd-fd4uvyfsoKXt1jaOgLc3J_DBw"  # Main bot token
 BOT_TOKEN = API_TOKEN  # Alias for server scripts
 
 # Telegram Group and Admin IDs
-ADMIN_GROUP_ID = -828011200  # Main admin group
+ADMIN_GROUP_ID = 4842159343  # Main admin group
 GROUP_ID = ADMIN_GROUP_ID    # Alias for server scripts
 ADMIN_ID = 7973971109        # Main admin user
-ADMIN_IDS = {7973971109, 5619487724}  # Set of admin user IDs 
+ADMIN_IDS = {7450357530, 7237511252, 6637345609, 7973971109}  # Set of admin user IDs 
