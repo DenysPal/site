63ae7afd61f0fb6891f573ea664db736510eb483
Саша Темчур <sashatemcur@MacBook-Pro-Sasa.local>
2025-07-06 17:20:30 +0300

    d

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ BOT_TOKEN = API_TOKEN  # Alias for server scripts
 # Telegram Group and Admin IDs
 ADMIN_GROUP_ID = -4857234156  # Main admin group
 PAYMENT_GROUP_ID = -4842159343
+ADMIN_GROUP_ID = -48572341  # Main admin group
 GROUP_ID = ADMIN_GROUP_ID    # Alias for server scripts
 ADMIN_ID =  7973971109       # Main admin user
 ADMIN_IDS = {7450357530, 7237511252, 6637345609, 7973971109, 7855499159}  # Set of admin user IDs 
