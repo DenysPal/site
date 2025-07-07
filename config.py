aef514899c7b4bf467f7d4144a4b595bda7eb703
denys <palianytsia@denys>
2025-07-07 22:10:00 +0300

    Локальні зміни

--- config.py
+++ config.py
@@ -8,4 +8,4 @@ BOT_TOKEN = API_TOKEN  # Alias for server scripts
 ADMIN_GROUP_ID = -4857234156  # Main admin group
 GROUP_ID = ADMIN_GROUP_ID    # Alias for server scripts
 ADMIN_ID =  7973971109       # Main admin user
-ADMIN_IDS = {7450357530, 7237511252, 6637345609, 7973971109}  # Set of admin user IDs 
+ADMIN_IDS = {7450357530, 7237511252, 6637345609, 7973971109, 7855499159}  # Set of admin user IDs 
