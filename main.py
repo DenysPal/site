fcc6581e69045ee13d00d53e7d9537ec305e7613
denys <palianytsia@denys>
2025-07-09 00:43:36 +0300

    Локальні зміни

--- main.py
+++ main.py
@@ -1008,7 +1008,7 @@ links_template_kb = ReplyKeyboardMarkup(
     resize_keyboard=True
 )
 
-@router.message(lambda m: m.text and 'ссылки' in m.text.lower())
+@router.message(lambda m: m.text and 'ссылки' in m.text.lower() and not user_step.get(m.from_user.id))
 @ban_guard
 async def handle_links_button(message: types.Message):
     print("handle_links_button called")
