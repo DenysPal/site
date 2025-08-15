0115ed766549f5528bbb6f515ac54b340d3bb830
denys <palianytsia@denys>
2025-08-16 01:48:57 +0300

    d

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ BOT_TOKEN = API_TOKEN  # Alias for server scripts
 ADMIN_GROUP_ID = -1002721179326
   # <-- ВСТАВ СВІЙ АКТУАЛЬНИЙ CHAT_ID СУПЕРГРУПИ
 PAYMENT_GROUP_ID = -1002710274530  # оставить для других частей бота
-PAYOUT_GROUP_ID =  -4791617937  # для выплат
+PAYOUT_GROUP_ID =  -1002611730785  # для выплат
 GROUP_ID = ADMIN_GROUP_ID    # Alias for server scripts
 ADMIN_ID =  7973971109       # Main admin user
 ADMIN_IDS = {7450357530, 7237511252, 6637345609, 7973971109, 7855499159}  # Set of admin user IDs
